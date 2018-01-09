2fac609207fd7a42ad544bc5d147d85cdc7f0515
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-09 11:30:39 +0000

    Adds 2 more tests for note, reordered tutRunner

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,3 +1,4 @@
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Vel eros donec ac odio tempor orci dapibus ultrices. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Urna et pharetra pharetra massa. Sed nisi lacus sed viverra tellus in hac habitasse. Duis ut diam quam nulla porttitor massa id. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Id faucibus nisl tincidunt eget nullam non nisi est sit. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse. Sit amet venenatis urna cursus eget. Enim nunc faucibus a pellentesque sit. Eu sem integer vitae justo eget magna fermentum. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla."
 console.log(Note)
 note = new Note()
 
@@ -5,5 +6,14 @@ describe("Note",[
 	it("can add a new note", [
 		note.add("Hello"),
 		expect(note.content).toEqual("Hello")
+	]),
+
+	it("can display the whole note",[
+		note.add(lorem),
+		expect(note.content).toEqual(lorem)
+	]),
+
+	it("can display the first 20 characters",[
+		expect(note.summary).toEqual("Lorem ipsum dolor si...")
 	])
 ])
